7805c08d98f6e3e360c6e8486c99f36241c214fb
dalsantosev <severinedalsanto@gmail.com>
2023-10-21 12:01:26 +0200

    modif post soutenance - underline acc and a prop

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,19 +1,20 @@
-import React from 'react';
-import { NavLink } from "react-router-dom"
+import React from "react";
+import { NavLink } from "react-router-dom";
+import "../App.css";
 
 const Navigation = () => {
-    return (
-        <div className="navigation">
-            <ul>
-                <NavLink to="/home" className='accueil'>
-                    <li>Accueil</li>
-                </NavLink>
-                <NavLink to="/about">
-                    <li>A propos</li>
-                </NavLink>
-            </ul>
-        </div>
-    );
+  return (
+    <div className="navigation">
+      <ul>
+        <NavLink to="/home" className="accueil" activeClassName="active">
+          <li>Accueil</li>
+        </NavLink>
+        <NavLink to="/about" className="accueil" activeClassName="active">
+          <li>A propos</li>
+        </NavLink>
+      </ul>
+    </div>
+  );
 };
 
 export default Navigation;
